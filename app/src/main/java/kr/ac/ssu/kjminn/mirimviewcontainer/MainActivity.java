4c55b2b7695c5a2cf299f6242688820f45a35a6e
kjminn <kjminn2001@gmail.com>
2020-11-03 21:37:05 +0900

    Complete ViewFlipper onTouchListener

--- app/src/main/java/kr/ac/ssu/kjminn/mirimviewcontainer/MainActivity.java
+++ app/src/main/java/kr/ac/ssu/kjminn/mirimviewcontainer/MainActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     ViewFlipper flipper;
+    float downX, upX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,26 @@ public class MainActivity extends AppCompatActivity {
         Button btnNext = findViewById(R.id.btn_next);
         btnPrev.setOnClickListener(btnListener);
         btnNext.setOnClickListener(btnListener);
+
+
+        flipper.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    downX = event.getX();
+                }else if(event.getAction() == MotionEvent.ACTION_UP){
+                    upX = event.getX();
+                    if(downX > upX){
+                        flipper.showNext();
+                    }else if(downX  < upX){
+                        flipper.showPrevious();
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(), (downX - upX) + "", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 
     View.OnClickListener btnListener = new View.OnClickListener() {
