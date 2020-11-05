16a1cbcb7b6fc354821c67a9c5a46ba4a433c2d1
kjminn <kjminn2001@gmail.com>
2020-11-05 19:56:48 +0900

    Complete ViewFlipper onTouchListener

--- app/src/main/java/kr/ac/ssu/kjminn/mirimviewcontainer/MainActivity.java
+++ app/src/main/java/kr/ac/ssu/kjminn/mirimviewcontainer/MainActivity.java
@@ -23,27 +23,28 @@ public class MainActivity extends AppCompatActivity {
         Button btnNext = findViewById(R.id.btn_next);
         btnPrev.setOnClickListener(btnListener);
         btnNext.setOnClickListener(btnListener);
+        flipper.setOnTouchListener(touchListener);
 
-
-        flipper.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction() == MotionEvent.ACTION_DOWN){
-                    downX = event.getX();
-                }else if(event.getAction() == MotionEvent.ACTION_UP){
-                    upX = event.getX();
-                    if(downX > upX){
-                        flipper.showNext();
-                    }else if(downX  < upX){
-                        flipper.showPrevious();
-                    }
-                }
-
-                Toast.makeText(getApplicationContext(), (downX - upX) + "", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
     }
+
+//    ★★★ XML ViewFlipper 요소에 clickable = true로 반드시 설정한다.
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent e) {
+            if(e.getAction() == MotionEvent.ACTION_DOWN){
+                downX = e.getX();
+            }else if(e.getAction() == MotionEvent.ACTION_UP){
+                upX = e.getX();
+                if(downX > upX){
+                    flipper.showNext();
+                }else if(downX < upX){
+                    flipper.showPrevious();
+                }
+            }
+
+            return false;
+        }
+    };
 
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
---
+++ app/src/main/java/kr/ac/ssu/kjminn/mirimviewcontainer/TabActivityTest.java
@@ -0,0 +1,30 @@
+package kr.ac.ssu.kjminn.mirimviewcontainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class TabActivityTest extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec tabSpecPark = tabHost.newTabSpec("PARK").setIndicator("박세리");
+        tabSpecPark.setContent(R.id.linear_park);
+        tabHost.addTab(tabSpecPark);
+
+        TabHost.TabSpec tabSpecSon = tabHost.newTabSpec("SON").setIndicator("손예진");
+        tabSpecSon.setContent(R.id.linear_son);
+        tabHost.addTab(tabSpecSon);
+
+        TabHost.TabSpec tabSpecAhn = tabHost.newTabSpec("AHN").setIndicator("안현모");
+        tabSpecAhn.setContent(R.id.linear_ahn);
+        tabHost.addTab(tabSpecAhn);
+
+        tabHost.setCurrentTab(0);
+    }
+}
